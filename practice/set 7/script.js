d17a260785d69e984907ff330ad38c35cce3a03b
Dhaval Antala <dhavalantala@Dhavals-MacBook-Pro.local>
2026-05-12 21:18:41 +0530

    Object and it's practice

---
+++ practice/set 7/script.js
@@ -0,0 +1,48 @@
+// let product = {
+//     companyName : `H&M`,
+//     item : `T-Shirt`,
+//     price : 865,
+//     diliveryTime : `2 Days`
+// }
+
+// let a = 5;
+// let b = a;
+// console.log(`a = ${a}, b = ${b}`)
+
+// a = 8
+// console.log(`a = ${a}, b = ${b}`)
+
+// x = {
+//     num : 5
+// };
+// y = x
+// console.log(`x = ${x.num}, y = ${y.num}`)
+
+// x['num'] = 8
+// console.log(`x = ${x.num}, y = ${y.num}`)
+
+
+// console.log(product['diliveryTime'])
+
+// let object = {
+//     message : `Good Job`,
+//     status : 'Complete'
+// }
+
+// let {message} = object
+// let {status} = object
+
+// console.log(message)
+// console.log(status)
+
+// function isIdenticalProduct(product1, product2) {
+//     if  (typeof product1 !== 'object' || typeof product2 !== 'object') {
+//         console.warn(`Parameter passed was not an object!`);
+//     }
+
+//     if (product1 == product2) {
+//         return true
+//     }
+// }
+
+// console.log(isIdenticalProduct(x, y))
