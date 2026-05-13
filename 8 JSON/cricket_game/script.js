b12070b15c1d0b3db0c21cb689c765d3ccb0d5c5
Dhaval Antala <dhavalantala@Dhavals-MacBook-Pro.local>
2026-05-13 10:44:03 +0530

    JSON DOM local Storage

--- 8 JSON/cricket_game/script.js
+++ 8 JSON/cricket_game/script.js
@@ -2,107 +2,105 @@ let scoreStr = localStorage.getItem('Score');
 let score;
 
 function resetScore(scoreStr) {
-    score = scoreStr ? JSON.parse(scoreStr) : {
-        win : 0,
-        lost : 0,
-        tie : 0,
-    };
+  score = scoreStr ? JSON.parse(scoreStr) : { win: 0, lost: 0, tie: 0 };
+  
+  score.displayScore = function() {
+    return `No of matches Won: ${this.win}, Lost: ${this.lost}, Tie: ${this.tie}`;
+  };
+  
+  // Passed empty strings so it doesn't break on initial load
+  showMethod('', '', ''); 
+}
 
-    score.displayScore = function() {
-        return `No of matches Won: ${this.win}, Lost: ${this.lost}, Tie: ${this.tie}`
-    };
-};
+resetScore(scoreStr);
 
-function generateComputerChoice(){
-    let randomNumber = Math.random() * 3;
-
-    if (randomNumber <= 1) {
-        return 'Bat';
-    } else if (randomNumber <= 2){
-        return 'Ball';
-    } else {
-        return'Stump';
-    }
+function generateComputerChoice() {
+  let randomNumber = Math.random() * 3;
+  if (randomNumber <= 1) {
+    return 'Bat';
+  } else if (randomNumber <= 2) {
+    return 'Ball';
+  } else {
+    return 'Stump';
+  }
 }
 
 function getResult(userMove, computerMove) {
-    if (userMove === `Ball`) {
-        if (computerMove === "Ball") {
-            score.tie++;
-            return `It's a tie` 
-        } else if (computerMove == 'Bat') {
-            score.lost++;
-            return `Computer has won!`
-        } else if (computerMove == 'Stump') {
-            score.win++;
-            return `User won.`
-        }
-    }else if (userMove === `Bat`) {
-        if (computerMove === "Ball") {
-            score.win++;
-            return `User won.`
-        } else if (computerMove == 'Bat') {
-            score.tie++;
-            return `It's a tie`
-        } else if (computerMove == 'Stump') {
-            score.lost++;
-            return `Computer has won!`
-        }
-    }else { 
-        if (computerMove === "Ball") {
-            score.lost++;
-            return `Computer has won!`
-        } else if (computerMove == 'Bat') {
-            score.win++;
-            return `User won.`
-        } else if (computerMove == 'Stump') {
-            score.tie++;
-            return`It's a tie` 
-        }
+  if (userMove === 'Ball') {
+    if (computerMove === 'Ball') {
+      score.tie++;
+      return "It's a tie";
+    } else if (computerMove === 'Bat') {
+      score.lost++;
+      return 'Computer has won!';
+    } else if (computerMove === 'Stump') {
+      score.win++;
+      return 'User won.';
     }
+  } else if (userMove === 'Bat') {
+    if (computerMove === 'Ball') {
+      score.win++;
+      return 'User won.';
+    } else if (computerMove === 'Bat') {
+      score.tie++;
+      return "It's a tie";
+    } else if (computerMove === 'Stump') {
+      score.lost++;
+      return 'Computer has won!';
+    }
+  } else {
+    if (computerMove === 'Ball') {
+      score.lost++;
+      return 'Computer has won!';
+    } else if (computerMove === 'Bat') {
+      score.win++;
+      return 'User won.';
+    } else if (computerMove === 'Stump') {
+      score.tie++;
+      return "It's a tie";
+    }
+  }
 }
 
-function showMethod(userChoiceMessage, computerChoicemsg, resultMsg){
-    localStorage.setItem('Score', JSON.stringify(score))
+function showMethod(userChoiceMessage, computerChoicemsg, resultMsg) {
+  localStorage.setItem('Score', JSON.stringify(score));
+  
+  document.querySelector('#user-move').innerText = userChoiceMessage ? `${userChoiceMessage}` : '';
+  document.querySelector('#computer-move').innerText = computerChoicemsg ? `${computerChoicemsg}` : '';
+  document.querySelector('#result').innerText = resultMsg ? `${resultMsg}` : '';
+  document.querySelector('#score').innerText = `${score.displayScore()}`;
+  
+  // Only alert if there is an actual message to display
+  if (userChoiceMessage && computerChoicemsg && resultMsg) {
     alert(`${userChoiceMessage}, ${computerChoicemsg}, and ${resultMsg}. ${score.displayScore()}`);
+  }
 }
 
-function bat(){
-    const userChoiceMessage =  'You have choosen Bat!'
-
-    let computerChoice = generateComputerChoice()
-    const computerChoicemsg = `Computer choice is ${computerChoice}!`;
-
-    let resultMsg = getResult(userMove = `Bat`, computerChoice);
-
-
-    
-    showMethod(userChoiceMessage, computerChoicemsg, resultMsg)
+function bat() {
+  const userChoiceMessage = 'You have chosen Bat!';
+  let computerChoice = generateComputerChoice();
+  const computerChoicemsg = `Computer choice is ${computerChoice}!`;
+  let resultMsg = getResult('Bat', computerChoice);
+  showMethod(userChoiceMessage, computerChoicemsg, resultMsg);
 }
 
-function ball(){
-    const userChoiceMessage =  'You have choosen Ball!'
-
-    let computerChoice = generateComputerChoice()
-    const computerChoicemsg = `Computer choice is ${computerChoice}!`;
-
-    let resultMsg = getResult(userMove = `Ball`, computerChoice);
-
-    showMethod(userChoiceMessage, computerChoicemsg, resultMsg)
+function ball() {
+  const userChoiceMessage = 'You have chosen Ball!';
+  let computerChoice = generateComputerChoice();
+  const computerChoicemsg = `Computer choice is ${computerChoice}!`;
+  let resultMsg = getResult('Ball', computerChoice);
+  showMethod(userChoiceMessage, computerChoicemsg, resultMsg);
 }
 
-function stump(){
-    const userChoiceMessage =  'You have choosen Stump!'
-
-    let computerChoice = generateComputerChoice()
-    const computerChoicemsg = `Computer choice is ${computerChoice}!`;
-    
-    let resultMsg = getResult(userMove = `Stump`, computerChoice);
-    
-    showMethod(userChoiceMessage, computerChoicemsg, resultMsg)
+function stump() {
+  const userChoiceMessage = 'You have chosen Stump!';
+  let computerChoice = generateComputerChoice();
+  const computerChoicemsg = `Computer choice is ${computerChoice}!`;
+  let resultMsg = getResult('Stump', computerChoice);
+  showMethod(userChoiceMessage, computerChoicemsg, resultMsg);
 }
 
 function rest() {
-    resetScore();
-    return localStorage.clear()
+  localStorage.clear();
+  resetScore(null); // Reset the local variable after clearing storage
 }
